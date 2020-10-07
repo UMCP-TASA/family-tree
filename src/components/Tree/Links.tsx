31b92d76303886ddab9f7099180e35c1b4c32195
Stephan Loh <sloh4137@gmail.com>
2020-10-06 20:37:44 -0400

    Add slight curve to link components

--- src/components/Tree/Links.tsx
+++ src/components/Tree/Links.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useTheme } from "@material-ui/core"
 import { LinkType } from "."
-import { useTransition, animated, to } from "react-spring"
+import { useTransition, animated } from "react-spring"
 import { findCollapsedParent, getX0, getY0 } from "@utils"
 
 interface TransitionOutput extends React.CSSProperties {
@@ -9,16 +9,28 @@ interface TransitionOutput extends React.CSSProperties {
     sy: number
     tx: number
     ty: number
+    opacity: number
 }
 
 type LinkProps = React.SVGAttributes<SVGPathElement> & TransitionOutput
 
 const Link = (props: LinkProps) => {
     const { sx, sy, tx, ty, ...rest } = props
+    const slope = tx - sx == 0 ? 0 : (ty - sy) / (tx - sx)
+    const b = sy - slope * sx
+    const offset = 100
+
+    const c1x = (tx - sx) / 3 + sx
+    const c1y = slope * c1x + b
+
+    const c2x = ((tx - sx) * 2) / 3 + sx
+    const c2y = slope * c2x + b
     return (
         <path
-            d={`M ${sy},${sx} L ${ty},${tx}
-    `}
+            d={`
+            M ${sy},${sx} 
+            C ${c1y + offset},${c1x} ${c2y - offset},${c2x} ${ty},${tx}
+            `}
             {...rest}
         />
     )
@@ -30,45 +42,44 @@ type LinksProps = {
     links: LinkType[]
 }
 
-
-
 export default function Links(props: LinksProps) {
     const { links } = props
     const theme = useTheme()
-    const transitions = useTransition<LinkType, TransitionOutput>(
-        links,
-        {
-            key: (link: LinkType) =>
-                `${link.source.data.attributes.id}-${link.target.data.attributes.id}`,
-            from: ({ source, target }) => ({
-                sx: getX0(source),
-                sy: getY0(source),
-                tx: getX0(target),
-                ty: getY0(target),
-            }),
-            enter: ({ source, target }) => ({
-                sx: source.x,
-                sy: source.y,
-                tx: target.x,
-                ty: target.y,
-            }),
-            update: ({ source, target }) => ({
-                sx: source.x,
-                sy: source.y,
-                tx: target.x,
-                ty: target.y,
-            }),
-            leave: ({ source }) => {
-                const collapsedParent = findCollapsedParent(source)
-                return {
-                    sx: getX0(collapsedParent),
-                    sy: getY0(collapsedParent),
-                    tx: getX0(collapsedParent),
-                    ty: getY0(collapsedParent),
-                }
-            },
-        }
-    )
+    const transitions = useTransition<LinkType, TransitionOutput>(links, {
+        key: (link: LinkType) =>
+            `${link.source.data.attributes.id}-${link.target.data.attributes.id}`,
+        from: ({ source, target }) => ({
+            sx: getX0(source),
+            sy: getY0(source),
+            tx: getX0(target),
+            ty: getY0(target),
+            opacity: 0,
+        }),
+        enter: ({ source, target }) => ({
+            sx: source.x,
+            sy: source.y,
+            tx: target.x,
+            ty: target.y,
+            opacity: 1,
+        }),
+        update: ({ source, target }) => ({
+            sx: source.x,
+            sy: source.y,
+            tx: target.x,
+            ty: target.y,
+            opacity: 1,
+        }),
+        leave: ({ source }) => {
+            const collapsedParent = findCollapsedParent(source)
+            return {
+                sx: getX0(collapsedParent),
+                sy: getY0(collapsedParent),
+                tx: getX0(collapsedParent),
+                ty: getY0(collapsedParent),
+                opacity: 0,
+            }
+        },
+    })
 
     return (
         <>
@@ -82,15 +93,16 @@ export default function Links(props: LinksProps) {
                     key={keyAccessor(link)}
                 />
             ))} */}
-            {transitions((style, item) => (
+            {transitions((style, __) => (
                 <AnimatedLink
                     sx={style.sx}
                     sy={style.sy}
                     tx={style.tx}
                     ty={style.ty}
+                    // @ts-ignore Not sure why stroke is invalid here
                     stroke={theme.palette.link.main}
+                    opacity={style.opacity}
                     strokeWidth="1"
-                    opacity={props.opacity}
                     fill="none"
                 />
             ))}
